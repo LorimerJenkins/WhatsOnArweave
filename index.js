81df9c1ea6af9bbb1bfc97debbc3cd386600a215
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-07-23 14:51:43 +0100

    feat: scrape arweave tags

---
+++ index.js
@@ -0,0 +1,20 @@
+import listenForTransactions from './query-arweave.js';
+import sendSlackMessage from './ping.js'
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+async function mainLoop() {
+  while (true) {
+    const transactionTags = await listenForTransactions();
+
+    await sendSlackMessage(transactionTags)
+
+    await delay(20000);
+  }
+}
+
+mainLoop().catch((error) => {
+  console.error('An error occurred:', error);
+});
+console.log('Whats On Arweave: **LIVE**')
+
---
+++ ping.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+
+export async function sendSlackMessage(message) {
+    await axios.post('https://slack.com/api/chat.postMessage', { 
+        channel: process.env.SLACK_CHANNEL_ID,
+        text: message
+    }, { headers: { 'Authorization': `Bearer ${process.env.SLACK_TOKEN}`, 'Content-Type': 'application/json' } });
+}
+
